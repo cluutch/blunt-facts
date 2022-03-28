f3781ab4447ce80f6a839c39e4e4465a45528b96
Cluutch API <code@cluutch.io>
2022-03-27 21:07:02 -0500

    feat: parameterize additional args

--- genbluntfactsnft_cf/main.py
+++ genbluntfactsnft_cf/main.py
@@ -57,6 +57,40 @@ def gen_blunt_facts_nft(request):
     else:
         return 'Invalid input_img_filename'
 
+    if request_json and 'title_width_pct' in request_json:
+        title_width_pct = request_json['title_width_pct']
+    elif request_args and 'title_width_pct' in request_args:
+        title_width_pct = request_args['title_width_pct']
+    else:
+        title_width_pct = 0.55
+    
+    if request_json and 'title_font_size' in request_json:
+        title_font_size = request_json['title_font_size']
+    elif request_args and 'title_font_size' in request_args:
+        title_font_size = request_args['title_font_size']
+    else:
+        title_font_size = 40
+    
+    if request_json and 'price_font_size' in request_json:
+        price_font_size = request_json['price_font_size']
+    elif request_args and 'price_font_size' in request_args:
+        price_font_size = request_args['price_font_size']
+    else:
+        price_font_size = 30
+    
+    if request_json and 'info_font_size' in request_json:
+        info_font_size = request_json['info_font_size']
+    elif request_args and 'info_font_size' in request_args:
+        info_font_size = request_args['info_font_size']
+    else:
+        info_font_size = 30
+
+    if request_json and 'nft_number_width_pct' in request_json:
+        nft_number_width_pct = request_json['nft_number_width_pct']
+    elif request_args and 'nft_number_width_pct' in request_args:
+        nft_number_width_pct = request_args['nft_number_width_pct']
+    else:
+        nft_number_width_pct = 0.03
 
     logging.debug("Requesting blunt facts info")
     blunt_facts_info = requests.get(BLUNT_FACTS_ENDPOINTS,
@@ -67,11 +101,11 @@ def gen_blunt_facts_nft(request):
     nft = BluntFactsNft(blunt_facts_info,
                         input_img_filename,
                         nft_number,
-                        title_font_size=30,
-                        price_font_size=24,
-                        info_font_size=24,
-                        nft_number_width_pct=0.01,
-                        title_width_pct=0.55,
+                        title_width_pct=title_width_pct,
+                        title_font_size=title_font_size,
+                        price_font_size=price_font_size,
+                        info_font_size=info_font_size,
+                        nft_number_width_pct=nft_number_width_pct,
                         use_cloud=True,
                         output_cloud_bucket=OUTPUT_CLOUD_BUCKET,
                         input_cloud_bucket=INPUT_CLOUD_BUCKET)
